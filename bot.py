c39d3ff9f8eb6aa2782b27f1acb88e85b63450f4
Daksh-Op21 <gamerdaksh19@gmail.com>
2024-07-29 17:57:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -52,7 +52,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL1 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL1}")
-                self.LOGGER(__name__).info("\nBot Stopped. https://t.me/MadflixBots_Support for support")
+                self.LOGGER(__name__).info("\nBot Stopped. Contact @iTz_Anayokoji for support")
                 sys.exit()
 
         if FORCE_SUB_CHANNEL2:
@@ -66,7 +66,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL2 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL2}")
-                self.LOGGER(__name__).info("\nBot Stopped. https://t.me/MadflixBots_Support for support")
+                self.LOGGER(__name__).info("\nBot Stopped. Contact @iTz_Anayokoji for support")
                 sys.exit()
 
         if FORCE_SUB_CHANNEL3:
@@ -80,7 +80,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL3 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL3}")
-                self.LOGGER(__name__).info("\nBot Stopped. https://t.me/MadflixBots_Support for support")
+                self.LOGGER(__name__).info("\nBot Stopped. Contact @iTz_Anayokoji for support")
                 sys.exit()
         
 
@@ -92,18 +92,22 @@ class Bot(Client):
         except Exception as e:
             self.LOGGER(__name__).warning(e)
             self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
-            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/MadflixBots_Support for support")
+            self.LOGGER(__name__).info("\nBot Stopped. Contact @iTz_Anayokoji for support")
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/Madflix_Bots")
-        self.LOGGER(__name__).info(f"""
-──╔╦══╦═══╦╗─╔╦╗─╔╦══╗╔═══╦════╦════╗
-──║╠╣╠╣╔═╗║║─║║║─║║╔╗║║╔═╗║╔╗╔╗╠══╗═║
-──║║║║║╚══╣╚═╝║║─║║╚╝╚╣║─║╠╝║║╚╝─╔╝╔╝
-╔╗║║║║╚══╗║╔═╗║║─║║╔═╗║║─║║─║║──╔╝╔╝
-║╚╝╠╣╠╣╚═╝║║─║║╚═╝║╚═╝║╚═╝║─║║─╔╝═╚═╗
-╚══╩══╩═══╩╝─╚╩═══╩═══╩═══╝─╚╝─╚════╝""")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \n@iTz_Anayokoji")
+        self.LOGGER(__name__).info(f"""\n\n
+        
+                
+ █████╗ ███╗   ██╗██╗███████╗██╗  ██╗██╗███╗   ██╗
+██╔══██╗████╗  ██║██║██╔════╝██║  ██║██║████╗  ██║
+███████║██╔██╗ ██║██║███████╗███████║██║██╔██╗ ██║
+██╔══██║██║╚██╗██║██║╚════██║██╔══██║██║██║╚██╗██║
+██║  ██║██║ ╚████║██║███████║██║  ██║██║██║ ╚████║
+╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝╚══════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝ 
+        
+        """)
         self.username = usr_bot_me.username
         #web-response
         app = web.AppRunner(await web_server())
